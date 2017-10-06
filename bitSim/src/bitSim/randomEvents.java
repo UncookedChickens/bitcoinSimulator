6eb94dfc3ce047564da5da5e59e2021def57671a
Wiebez <zacattac5435@gmil.com>
2017-10-06 09:44:08 -0500

    randoms

--- bitSim/src/bitSim/randomEvents.java
+++ bitSim/src/bitSim/randomEvents.java
@@ -1,9 +1,13 @@
 package bitSim;
 
+import java.util.Random;
+
 public class randomEvents {
 
 	public static void randomEvent() {
+		Random r = new Random();
 		// This is where random events are made
+		
 	}
 
 }
