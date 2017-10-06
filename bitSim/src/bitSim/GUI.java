d35e9a8e3351c62e355f7422fe57802d0375e137
BaranuikN <BaranuikN@WCMNLABS020LT.blads.ca>
2017-10-06 09:50:03 -0500

    created a frame

--- bitSim/src/bitSim/GUI.java
+++ bitSim/src/bitSim/GUI.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
 
 public class GUI extends JFrame {
 
@@ -32,11 +33,11 @@ public class GUI extends JFrame {
 	 */
 	public GUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 900, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 
 }
