2d2ffae9942974c7caecdd475561a316f6569563
BaranuikN <BaranuikN@WCMNLABS020LT.blads.ca>
2017-10-10 09:51:44 -0500

    I dunno, something

--- bitSim/src/bitSim/GUI.java
+++ bitSim/src/bitSim/GUI.java
@@ -7,10 +7,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class GUI extends JFrame {
-
+	
+	
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -32,12 +36,21 @@ public class GUI extends JFrame {
 	 * Create the frame.
 	 */
 	public GUI() {
+		setTitle("I dunno, something");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 900, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(12, 13, 625, 427);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Next Week");
+		btnNewButton.setBounds(709, 30, 97, 43);
+		contentPane.add(btnNewButton);
 	}
-
 }
