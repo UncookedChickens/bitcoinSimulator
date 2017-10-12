109b69cfdc840a50fff487972a022c5bacc971d6
Wiebez <zacattac5435@gmil.com>
2017-10-12 09:49:29 -0500

    gui

--- bitSim/src/bitSim/GUI.java
+++ bitSim/src/bitSim/GUI.java
@@ -2,15 +2,23 @@ package bitSim;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
-public class GUI extends JFrame {
-
+public class GUI extends JFrame implements ActionListener {
+	
+	
 	private JPanel contentPane;
+	private JTextField textField;
+	private String text;
+	private int price, week;
 
 	/**
 	 * Launch the application.
@@ -32,12 +40,46 @@ public class GUI extends JFrame {
 	 * Create the frame.
 	 */
 	public GUI() {
+		text = "Week ";
+		week = 0;
+		price = 4500;
+		
+		setTitle("I dunno, something");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 900, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(12, 13, 625, 427);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		textField.equals(text);
+		
+		JButton btn = new JButton("Next Week");
+		btn.setActionCommand("btn");
+		btn.addActionListener(this);
+		btn.setBounds(709, 30, 97, 43);
+		contentPane.add(btn);
 	}
-
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		randomEvents event = new randomEvents();
+		String eventName = e.getActionCommand();
+		
+		
+	
+		for(int i = 0; i < 52; i++){
+			if(eventName.equals("btn")){
+				event.randomEvent(price);//makes starting price random
+				week ++;
+				
+				textField.equals(text +week +": " +price);
+				
+			}
+		}
+	}
 }
