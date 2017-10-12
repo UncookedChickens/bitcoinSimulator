b95bc56b46f7822496ab2f0da8384ceff448a111
Wiebez <zacattac5435@gmil.com>
2017-10-12 09:49:56 -0500

    random events

--- bitSim/src/bitSim/randomEvents.java
+++ bitSim/src/bitSim/randomEvents.java
@@ -1,9 +1,39 @@
 package bitSim;
 
+import java.util.Random;
+
 public class randomEvents {
 
-	public static void randomEvent() {
+	public static int randomEvent(int price) {
+		Random r = new Random();
+		int chance, change=0;
+		chance = r.nextInt(100)+1;
 		
+		if(chance<=4){
+			change = r.nextInt(1500-500+1) + 500;
+		}
+		
+		if(chance==1 || chance==2){
+			change = change * -1;
+		}
+		
+		return change;
+	}
+	
+	public static int trend(int price) {
+		Random r = new Random();
+		int change, direction;
+		
+		change = r.nextInt(501);
+		direction = r.nextInt(25) + 1;
+		
+		if(direction<10){
+			change = change * -1;
+		}
+		
+		price += change;
+		
+		return price;
 	}
 
 }
